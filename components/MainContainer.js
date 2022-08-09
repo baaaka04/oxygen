899ee60b92525686fb3eb719d883b24fe2ebdb6f
ab <ab@sdflkj.csm>
2022-08-09 14:31:59 +0300

    feat: add submit btn, isolate login page

--- components/MainContainer.js
+++ components/MainContainer.js
@@ -8,7 +8,6 @@ export default function MainContainer({ children }) {
             </div>
             <div className="fixed bottom-0 flex items-center justify-around w-full h-16 bg-slate-600">
                 <A href={'/'} text={'Home'} />
-                <A href={'/login'} text={'Login'} />
                 <A href={'/users'} text={'Users'} />
             </div>
         </div>
--- pages/login.js
+++ pages/login.js
@@ -18,23 +18,22 @@ const Login = () => {
             .then(() => Router.push('/'))
             .catch(err => {
                 console.log(err)
-                setPswValue('')
             })
+        setPswValue('')
     }
 
     return (
-        <MainContainer>
-            <form className="flex flex-col items-center p-40" onSubmit={hadleSubmit}>
-                <h1 className="mb-4 text-xl">Password</h1>
-                <input
-                    className="h-9 bg-slate-700"
-                    type="password"
-                    name="password"
-                    value={pswValue}
-                    onChange={(e) => setPswValue(e.target.value)}
-                />
-            </form>
-        </MainContainer>
+        <form className="flex flex-col items-center p-40" onSubmit={hadleSubmit}>
+            <h1 className="mb-4 text-xl">password</h1>
+            <input
+                className="pl-3 w-70 h-9 bg-slate-700"
+                type="password"
+                name="password"
+                value={pswValue}
+                onChange={(e) => setPswValue(e.target.value)}
+            />
+            <button className="px-5 mt-4 text-xl border rounded-lg">submit</button>
+        </form>
     );
 };
 
